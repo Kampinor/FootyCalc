df9c866733ac3e192c70daabb027a76ee7c32fc2
Kampinor <112555114+Kampinor@users.noreply.github.com>
2023-09-27 19:47:48 +0200

    -Hinzufügen Ergebnisse BL 1-5 2023/2024

---
+++ DB/Results-Inserts.sql
@@ -0,0 +1,50 @@
+-- Spieltage 1-5 Bundesliga
+INSERT INTO Results Values (0,13,1,0,0,13,1,0,4,0.6,2.913,13,1,0,0);
+INSERT INTO Results Values (1,6,3,0,0,6,3,3,2,1.4,1.66,6,1,0,0);
+INSERT INTO Results Values (2,7,19,0,0,7,19,2,0,2.2,1.27,7,1,0,0);
+INSERT INTO Results Values (3,12,14,0,0,12,14,5,0,3.1,0.812,12,1,0,0);
+INSERT INTO Results Values (4,15,10,0,0,15,10,3,3,1.5,0.915,15,1,0,0);
+INSERT INTO Results Values (5,11,5,0,0,11,5,0,0,1.4,2.711,11,1,0,0);
+INSERT INTO Results Values (6,2,9,0,0,2,9,1,0,2.1,1.32,2,1,0,0);
+INSERT INTO Results Values (7,4,8,0,0,4,8,4,0,1.9,0.74,4,1,0,0);
+INSERT INTO Results Values (8,0,18,0,0,0,18,1,0,1.5,0.60,0,1,0,0);
+
+INSERT INTO Results Values (10,3,12,0,0,3,12,3,1,1.8,1.13,3,2,0,0);
+INSERT INTO Results Values (11,14,2,0,0,14,2,1,1,1.0,1.314,14,2,0,0);
+INSERT INTO Results Values (12,18,4,0,0,18,4,0,3,2.3,1.718,18,2,0,1);
+INSERT INTO Results Values (13,5,13,0,0,5,13,1,0,2.7,1.15,5,2,0,0);
+INSERT INTO Results Values (14,9,7,0,0,9,7,1,1,0.9,1.79,9,2,0,0);
+INSERT INTO Results Values (15,19,11,0,0,19,11,1,1,1.7,2.219,19,2,0,0);
+INSERT INTO Results Values (16,10,6,0,0,10,6,0,3,0.9,2.810,10,2,0,0);
+INSERT INTO Results Values (17,8,0,0,0,8,0,0,1,1.2,1.18,8,2,0,1);
+INSERT INTO Results Values (18,1,15,0,0,1,15,1,1,2.1,0.71,1,2,0,0);
+
+INSERT INTO Results Values (19,2,19,0,0,2,19,1,1,2.6,1.62,2,3,0,0);
+INSERT INTO Results Values (20,13,8,0,0,13,8,3,0,2.0,0.513,13,3,0,0);
+INSERT INTO Results Values (21,11,7,0,0,11,7,3,1,2.0,1.211,11,3,0,0);
+INSERT INTO Results Values (22,6,18,0,0,6,18,5,1,2.8,0.46,6,3,0,0);
+INSERT INTO Results Values (23,15,14,0,0,15,14,1,2,2.2,1.715,15,3,0,0);
+INSERT INTO Results Values (24,12,5,0,0,12,5,5,0,1.7,1.112,12,3,0,0);
+INSERT INTO Results Values (25,10,1,0,0,10,1,1,2,0.3,2.410,10,3,0,0);
+INSERT INTO Results Values (26,0,9,0,0,0,9,1,0,1.4,0.10,0,3,0,0);
+INSERT INTO Results Values (27,4,3,0,0,4,3,0,3,0.3,1.24,4,3,1,0);
+
+INSERT INTO Results Values (28,1,6,0,0,1,6,1,0,2.1,1.31,1,4,0,0);
+INSERT INTO Results Values (29,7,4,0,0,7,4,1,1,0.4,1.17,7,4,0,0);
+INSERT INTO Results Values (30,8,12,0,0,8,12,1,3,1.0,1.68,8,4,0,0);
+INSERT INTO Results Values (31,5,2,0,0,5,2,2,4,1.1,2.45,5,4,1,0);
+INSERT INTO Results Values (32,9,11,0,0,9,11,1,1,1.0,1.09,9,4,1,0);
+INSERT INTO Results Values (33,3,15,0,0,3,15,3,0,1.1,1.73,3,4,0,0);
+INSERT INTO Results Values (34,14,0,0,0,14,0,0,0,1.3,0.614,14,4,0,0);
+INSERT INTO Results Values (35,19,13,0,0,19,13,3,1,1.5,2.019,19,4,0,0);
+INSERT INTO Results Values (36,18,10,0,0,18,10,3,2,1.2,3.118,18,4,1,0);
+
+INSERT INTO Results Values (37,12,18,0,0,12,18,3,0,2.3,0.212,12,5,0,0);
+INSERT INTO Results Values (38,1,14,0,0,1,14,5,0,4.3,0.11,1,5,0,0);
+INSERT INTO Results Values (39,4,11,0,0,4,11,0,1,1.6,1.24,4,5,0,0);
+INSERT INTO Results Values (40,15,8,0,0,15,8,1,1,0.8,0.615,15,5,1,0);
+INSERT INTO Results Values (41,2,7,0,0,2,7,1,0,1.7,0.52,2,5,0,0);
+INSERT INTO Results Values (42,10,3,0,0,10,3,0,0,0.8,0.710,10,5,0,0);
+INSERT INTO Results Values (43,13,9,0,0,13,9,2,0,2.1,1.513,13,5,0,0);
+INSERT INTO Results Values (44,6,19,0,0,6,19,3,1,2.8,0.56,6,5,0,0);
+INSERT INTO Results Values (45,0,5,0,0,0,5,0,0,0.6,0.60,0,5,0,0);
